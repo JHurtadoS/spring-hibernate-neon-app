5e47a8c7775078e494281e8d3cd183de9bc6fc1f
Juan Esteban Hurtado <jhurtados@car.gov.co>
2025-01-13 11:07:51 -0500

    Initial commit

---
+++ src/main/java/com/example/springhibernateneonapp/SpringHibernateNeonAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhibernateneonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateNeonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateNeonAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhibernateneonapp/SpringHibernateNeonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhibernateneonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateNeonAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
